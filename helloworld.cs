5063ff57c6d3858c40ede25e4b5c0ec5501246be
IsaS2 <168777599+isabellis2@users.noreply.github.com>
2026-06-17 08:39:13 -0300

    Add files via upload

---
+++ helloworld.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace MeuPrimeiroPrograma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
